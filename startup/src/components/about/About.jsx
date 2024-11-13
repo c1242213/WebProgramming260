054c3aa05b96e9d387a15704bceba90ef6080e52
Carlee Hansen <chansen@unc.edu>
2024-11-12 20:38:22 -0700

    finished about and login

---
+++ startup/src/components/about/About.jsx
@@ -0,0 +1,36 @@
+
+export default About;
+
+import React from 'react';
+import './about.css';
+
+export function About(props) {
+  const [imageUrl, setImageUrl] = React.useState('data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=');
+  const [quote, setQuote] = React.useState('Loading...');
+  const [quoteAuthor, setQuoteAuthor] = React.useState('unknown');
+
+  // We only want this to render the first time the component is created and so we provide an empty dependency list.
+  React.useEffect(() => {
+    setImageUrl(`placeholder.jpg`);
+    setQuote('Show me the code');
+    setQuoteAuthor('Linus Torvalds');
+  }, []);
+
+  return (
+    <main className='container-fluid bg-secondary text-center'>
+      <div>
+        <div id='picture' className='picture-box'>
+          <img src={imageUrl} alt='random image' />
+        </div>
+
+        <p>Picture perfect is an online coloring book that allows the artist to select a picture to color.</p>
+        <p>Start by choosing a picture and then a color. Each section of the picture you can choose a different color.</p>
+
+        <div className='quote-box bg-light text-dark'>
+          <p className='quote'>{quote}</p>
+          <p className='author'>{quoteAuthor}</p>
+        </div>
+      </div>
+    </main>
+  );
+}
