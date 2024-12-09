9f50fcec6812ecda69e4395bcfab7ea61c510f0d
Carlee Hansen <chansen@unc.edu>
2024-12-09 16:52:14 -0700

    adjusted my new play elements

---
+++ startup/src/app.jsx
@@ -0,0 +1,187 @@
+// // src/App.jsx
+// import React from 'react';
+// import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+// import { Login } from login
+// import { Play } from './play/Play';
+// import { Gallery } from './gallery/Gallery';
+// import { About } from './about/About';
+// import { AuthState } from './login/authState';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+// import './app.css';
+
+// function App() {
+//   const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+//   const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+//   const [authState, setAuthState] = React.useState(currentAuthState);
+
+//   return (
+//     <BrowserRouter>
+//       <div className="body bg-dark text-light">
+//         <header className="container-fluid">
+//           <nav className="navbar fixed-top navbar-dark">
+//             <div className="navbar-brand">
+//               Picture Perfect<sup>&reg;</sup>
+//             </div>
+//             <menu className="navbar-nav">
+//               <li className="nav-item">
+//                 <NavLink className="nav-link" to="/">
+//                   Login
+//                 </NavLink>
+//               </li>
+//               {authState === AuthState.Authenticated && (
+//                 <li className="nav-item">
+//                   <NavLink className="nav-link" to="/play">
+//                     Play
+//                   </NavLink>
+//                 </li>
+//               )}
+//               {authState === AuthState.Authenticated && (
+//                 <li className="nav-item">
+//                   <NavLink className="nav-link" to="/gallery">
+//                     Gallery
+//                   </NavLink>
+//                 </li>
+//               )}
+//               <li className="nav-item">
+//                 <NavLink className="nav-link" to="/about">
+//                   About
+//                 </NavLink>
+//               </li>
+//             </menu>
+//           </nav>
+//         </header>
+
+//         <main className="container-fluid bg-secondary text-center">
+//           <Routes>
+//             <Route
+//               path="/"
+//               element={
+//                 <Login
+//                   userName={userName}
+//                   authState={authState}
+//                   onAuthChange={(userName, authState) => {
+//                     setAuthState(authState);
+//                     setUserName(userName);
+//                   }}
+//                 />
+//               }
+//               exact
+//             />
+//             <Route path="/play" element={<Play userName={userName} />} />
+//             <Route path="/gallery" element={<Gallery />} />
+//             <Route path="/about" element={<About />} />
+//             <Route path="*" element={<NotFound />} />
+//           </Routes>
+//         </main>
+
+//         <footer className="bg-dark text-muted">
+//           <div className="container-fluid">
+//             <span className="text-reset">Author Name(s)</span>
+//             <a className="text-reset" href="https://github.com/webprogramming260/picture-perfect">
+//               Source
+//             </a>
+//           </div>
+//         </footer>
+//       </div>
+//     </BrowserRouter>
+//   );
+// }
+
+// function NotFound() {
+//   return <main className="container-fluid bg-secondary text-center">404: Page Not Found</main>;
+// }
+
+// export default App;
+
+
+import React from 'react';
+import { BrowserRouter, NavLink, Route, Routes } from 'react-router-dom';
+import { Login } from '../login/oldLogin/login';
+import { Play } from '../play/play';
+import { Scores } from './gallery/Gallery';
+import { About } from './about/About';
+import { AuthState } from '../login/oldLogin/authState';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './app.css';
+
+function App() {
+  const [userName, setUserName] = React.useState(localStorage.getItem('userName') || '');
+  const currentAuthState = userName ? AuthState.Authenticated : AuthState.Unauthenticated;
+  const [authState, setAuthState] = React.useState(currentAuthState);
+
+  return (
+    <BrowserRouter>
+      <div className='body bg-dark text-light'>
+        <header className='container-fluid'>
+          <nav className='navbar fixed-top navbar-dark'>
+            <div className='navbar-brand'>
+              Simon<sup>&reg;</sup>
+            </div>
+            <menu className='navbar-nav'>
+              <li className='nav-item'>
+                <NavLink className='nav-link' to=''>
+                  Login
+                </NavLink>
+              </li>
+              {authState === AuthState.Authenticated && (
+                <li className='nav-item'>
+                  <NavLink className='nav-link' to='play'>
+                    Play
+                  </NavLink>
+                </li>
+              )}
+              {authState === AuthState.Authenticated && (
+                <li className='nav-item'>
+                  <NavLink className='nav-link' to='scores'>
+                    Scores
+                  </NavLink>
+                </li>
+              )}
+              <li className='nav-item'>
+                <NavLink className='nav-link' to='about'>
+                  About
+                </NavLink>
+              </li>
+            </menu>
+          </nav>
+        </header>
+
+        <Routes>
+          <Route
+            path='/'
+            element={
+              <Login
+                userName={userName}
+                authState={authState}
+                onAuthChange={(userName, authState) => {
+                  setAuthState(authState);
+                  setUserName(userName);
+                }}
+              />
+            }
+            exact
+          />
+          <Route path='/play' element={<Play userName={userName} />} />
+          <Route path='/scores' element={<Scores />} />
+          <Route path='/about' element={<About />} />
+          <Route path='*' element={<NotFound />} />
+        </Routes>
+
+        <footer className='bg-dark text-dark text-muted'>
+          <div className='container-fluid'>
+            <span className='text-reset'>Author Name(s)</span>
+            <a className='text-reset' href='https://github.com/webprogramming260/simon-react'>
+              Source
+            </a>
+          </div>
+        </footer>
+      </div>
+    </BrowserRouter>
+  );
+}
+
+function NotFound() {
+  return <main className='container-fluid bg-secondary text-center'>404: Return to sender. Address unknown.</main>;
+}
+
+export default App;
