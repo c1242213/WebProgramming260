25d6b1a3e94ecb5a8b0434a106d9e08dfb4fb193
Carlee Hansen <chansen@unc.edu>
2024-12-11 22:06:53 -0700

    fixed about

--- startup/src/about/about.jsx
+++ startup/src/about/about.jsx
@@ -8,20 +8,42 @@ export function About(props) {
 
   // We only want this to render the first time the component is created and so we provide an empty dependency list.
   React.useEffect(() => {
-    setImageUrl(`placeholder.jpg`);
-    setQuote('Show me the code');
-    setQuoteAuthor('Linus Torvalds');
+    const random = Math.floor(Math.random() * 1000);
+    fetch(`https://picsum.photos/v2/list?page=${random}&limit=1`)
+      .then((response) => response.json())
+      .then((data) => {
+        const containerEl = document.querySelector('#picture');
+
+        const width = containerEl.offsetWidth;
+        const height = containerEl.offsetHeight;
+        const apiUrl = `https://picsum.photos/id/${data[0].id}/${width}/${height}?grayscale`;
+        setImageUrl(apiUrl);
+      })
+      .catch();
+
+    fetch('https://quote.cs260.click')
+      .then((response) => response.json())
+      .then((data) => {
+        setQuote(data.quote);
+        setQuoteAuthor(data.author);
+      })
+      .catch();
   }, []);
+
+  let imgEl = '';
+
+  if (imageUrl) {
+    imgEl = <img src={imageUrl} alt='stock background' />;
+  }
 
   return (
     <main className='container-fluid bg-secondary text-center'>
       <div>
         <div id='picture' className='picture-box'>
-          <img src={imageUrl} alt='random image' />
+          {imgEl}
         </div>
-
         <p>Picture perfect is an online coloring book that allows the artist to select a picture to color.</p>
-        <p>Start by choosing a picture and then a color. Each section of the picture you can choose a different color.</p>
+<p>Start by choosing a picture and then a color. Each section of the picture you can choose a different color.</p>
 
 
         <div className='quote-box bg-light text-dark'>
@@ -32,3 +54,5 @@ export function About(props) {
     </main>
   );
 }
+
+
