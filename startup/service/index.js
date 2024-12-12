03da2a32aa4d32967df57ba2770eeae4a60b09fd
Carlee Hansen <chansen@unc.edu>
2024-12-11 19:01:35 -0700

    used simon const for index.js

--- startup/service/index.js
+++ startup/service/index.js
@@ -1,4 +1,6 @@
-
+const express = require('express');
+const uuid = require('uuid');
+const app = express();
 
 // The scores and users are saved in memory and disappear whenever the service is restarted.
 let users = {};
